3087a6e315e6edd9072c168f366fce8741a0cb7c
hugoayd31 <147941046+hugoayd31@users.noreply.github.com>
2023-10-21 15:50:46 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,11 @@
-package Student;
-
+package com.example.Ftp_.Book.Student;
+import com.example.Ftp_.Book.Book;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
+
 
 @Setter
 @Getter
@@ -10,16 +13,8 @@ import lombok.*;
 @Table(name = "student")
 @Data
 public class Student {
-   @Id
-    @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
-    )
+    @Id
+    @GeneratedValue
     private Integer id;
     @Column(name = "first_name")
     private String firstName;
@@ -50,11 +45,5 @@ public class Student {
 
     }
 
-
-    public Student(String firstName, String lastName, String email, Integer age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.age = age;
-    }
 }
+
