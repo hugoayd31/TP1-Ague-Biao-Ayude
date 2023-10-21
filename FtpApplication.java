eaa965230d251b00b4c2225ed041b793db3e84b5
FadBiao <67750675+FadBiao@users.noreply.github.com>
2023-10-21 23:35:14 +0200

    Update DemoApplication.java

--- FtpApplication.java
+++ FtpApplication.java
@@ -1,13 +1,24 @@
-package Student;
+package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
-public class FtpApplication {
+@RestController
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FtpApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
 }
+
