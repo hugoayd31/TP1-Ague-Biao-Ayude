0089eeb5941ff06a63616bc7a2b534788e6b33b0
FadBiao <67750675+FadBiao@users.noreply.github.com>
2023-10-21 07:24:16 +0200

    Update StudentRepository.java
    just what we need in the repository file

--- StudentRepository.java
+++ StudentRepository.java
@@ -1,6 +1,5 @@
-package Student;
+package com.example.demo.student;
 
-import Student.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,15 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-    public List<Student> findAll();
-    public Optional<Student> findById(Integer id);
-    public Optional<Student> findByEmail(String email);
-    public Optional<Student> findByFirstNameAndLastName(String firstName,
-                                                        String lastName);
-    @Query(
-            value = "SELECT * FROM student u WHERE u.age > 20",
-            nativeQuery = true)
-    public List<Student> findByAge(Integer age);
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    @Query("select s from Student  s where s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+    @Query("select s from Student  s where s.age > ?1")
+    List <Student> findAllByAge(Integer age);
+
 
 }
