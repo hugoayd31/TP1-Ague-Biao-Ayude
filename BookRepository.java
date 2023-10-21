bc0961f5c0c67014b818c9c887111757c69c59a6
FadBiao <67750675+FadBiao@users.noreply.github.com>
2023-10-21 18:42:23 +0200

    Update BookRepository.java

--- BookRepository.java
+++ BookRepository.java
@@ -1,6 +1,6 @@
-package Book;
+package com.example.demo.book;
 
-import Student.Student;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,17 +8,14 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book,Integer> {
-    public List<Book> findAll();
-    public Optional<Book> findById(Integer id);
-    public Optional<Book> findByCode(Integer code);
+    @Repository
+    public interface BookRepository extends JpaRepository<Book,Integer> {
+        public List<Book> findAll();
+        public Optional<Book> findById(Integer id);
+        @Query("select b from Book b where b.code= ?1")
+        public Optional<Book> findByCode(Integer code);
 
-    public Optional<Book> findByName(String name);
+        public Optional<Book> findByName(String name);
 
-   // @Query(
-     //       value = "SELECT * FROM student u WHERE u.age > 20",
-       //     nativeQuery = true)
-    //public List<Student> findByAge(Integer age);
+    }
 
-}
