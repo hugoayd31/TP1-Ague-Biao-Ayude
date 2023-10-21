784ac31cc8c75564a178470a2c66ac0675a56097
FadBiao <67750675+FadBiao@users.noreply.github.com>
2023-10-21 07:59:46 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -10,8 +10,16 @@ import lombok.*;
 @Table(name = "student")
 @Data
 public class Student {
-    @Id
-    @GeneratedValue
+   @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Integer id;
     @Column(name = "first_name")
     private String firstName;
@@ -42,4 +50,11 @@ public class Student {
 
     }
 
+
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
 }
